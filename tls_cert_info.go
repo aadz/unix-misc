042ec16549c25a1507bbf88b3b68da4a241bff92
Alexander Kozlikhin <Alexander.Kozlikhin@gmail.com>
2016-09-23 00:01:03 +0300

    SHA1 certificate fingerprint added to info

--- tls_cert_info.go
+++ tls_cert_info.go
@@ -1,27 +1,30 @@
 /*
 by aadz, 2016
-TLS sertificate information grabber. Example of run:
+TLS certificate information grabber. Example of run:
 
-$ ./tls_cert_info www.gmail.com:443
-2016/09/22 21:15:09 Connected to www.gmail.com:443
+$ ./tls_cert_info www.google.com:443
+2016/09/22 23:55:20 Connected to www.google.com:443
 *** Certificate info:
 Version:        3
-Host:           [www.gmail.com]
-NotBefore:      2016-09-14 08:22:49 +0000 UTC
+Host:           [www.google.com]
+Fingerprint:    90:86:a4:3b:f5:cf:1b:2e:4e:f7:97:96:f9:de:ba:b9:66:35:86:3f
+NotBefore:      2016-09-14 08:20:40 +0000 UTC
 NotAfter:       2016-12-07 08:19:00 +0000 UTC
 *** Note:
-It is 2016-09-22 21:15:09.927378879 +0300 MSK now: 75 days left
+It is 2016-09-22 23:55:20.646289871 +0300 MSK now: 75 days left
 
 */
 
 package main
 
 import (
+	"crypto/sha1"
 	"crypto/tls"
 	"fmt"
 	"log"
 	"math"
 	"os"
+	"strings"
 	"time"
 )
 
@@ -33,10 +36,9 @@ func main() {
 	ssl_server := os.Args[1]
 
 	// Connect to server
-	tls_cfg := tls.Config{InsecureSkipVerify: true}
-	conn, err := tls.Dial("tcp", ssl_server, &tls_cfg)
+	conn, err := tls.Dial("tcp", ssl_server, &tls.Config{InsecureSkipVerify: true})
 	if err != nil {
-		log.Printf("Canot connect to %v : %v", ssl_server, err)
+		log.Printf("Canot connect to %v: %v", ssl_server, err)
 		os.Exit(1)
 	}
 	defer conn.Close()
@@ -44,9 +46,17 @@ func main() {
 
 	// Show server's certificate info
 	crt := conn.ConnectionState().PeerCertificates[0]
-	fmt.Printf("*** Certificate info:\nVersion:\t%v\nHost:\t\t%v\nNotBefore:\t%v\nNotAfter:\t%v\n",
-		crt.Version, crt.DNSNames, crt.NotBefore, crt.NotAfter)
 	days_left := math.Ceil(crt.NotAfter.Sub(time.Now()).Seconds()/86400) - 1
+
+	// Make fingerprint
+	sha1Sum := sha1.Sum(crt.Raw)	// a bytes array
+	Fingerprint := make([]string, len(sha1Sum))
+	for i, b := range sha1Sum {
+		Fingerprint[i] = fmt.Sprintf("%x", b)
+	}
+
+	fmt.Printf("*** Certificate info:\nVersion:\t%v\nHost:\t\t%v\nFingerprint:\t%v\nNotBefore:\t%v\nNotAfter:\t%v\n",
+		crt.Version, crt.DNSNames, strings.Join(Fingerprint, ":"), crt.NotBefore, crt.NotAfter)
 	fmt.Printf("*** Note:\nIt is %v now: %v days left\n", time.Now(), days_left)
 }
 
