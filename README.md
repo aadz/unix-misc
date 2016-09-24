acd9645cf485fd11b712f2705ce983edb9e278d5
Alexander Kozlikhin <Alexander.Kozlikhin@gmail.com>
2016-09-24 10:08:19 +0300

    Added support of IPv6 servers addresses and UTF domain names.

--- README.md
+++ README.md
@@ -4,18 +4,23 @@
 Script checks if SSL certificate expired or will expire soon and send notification by email
 
 ## tls_cert_info.go
-TLS certificate information grabber. Example of run:
+TLS certificate information grabber supporting IPv6 server adresses as well as UTF domain names.
+
+Example of run:
 ```
-$ ./tls_cert_info www.google.ru:443
-Connected to www.google.ru:443
+$ ./tls_cert_info example.com
+Connected to example.com:443
 *** Certificate info:
+IssuerCN:       DigiCert SHA2 High Assurance Server CA
 Version:        3
-Host:           [*.google.com.ru *.google.ru google.com.ru google.ru]
-NotBefore:      2016-09-14 08:25:39 +0000 UTC
-NotAfter:       2016-12-07 08:19:00 +0000 UTC
+SerialNum:      e64c5fbc236ade14b172aeb41c78cb0
+PubKey:         RSA Encryption
+CrtSign:        SHA256 With RSA Encryption
+NotBefore:      2015-11-03 00:00:00 +0000 UTC
+NotAfter:       2018-11-28 12:00:00 +0000 UTC - 795 days left
+SubjectCN:      www.example.org
+DNSNames:       [www.example.org example.com example.edu example.net example.org www.example.com www.example.edu www.example.net]
 *** Fingerprints:
-sha1:           d3:fa:53:d1:38:13:d2:14:b5:48:7d:d8:9f:c6:5b:ac:e0:c6:51:d3
-sha256:         e3:5e:14:c4:3d:49:20:d1:69:3f:a1:44:bb:f2:e4:d1:0a:fa:59:c2:88:35:ff:de:d7:08:bc:b5:cc:22:35:b5
-*** Note:
-It is 2016-09-23 01:21:24.561347678 +0300 MSK now: 75 validity days left
+sha1:           25:09:fb:22:f7:67:1a:ea:2d:0a:28:ae:80:51:6f:39:0d:e0:ca:21
+sha256:         64:2d:e5:4d:84:c3:04:94:15:7f:53:f6:57:bf:9f:89:b4:ea:6c:8b:16:35:1f:d7:ec:25:8d:55:6f:82:10:40
 ```
--- tls_cert_info.go
+++ tls_cert_info.go
@@ -9,54 +9,178 @@ import (
 	"crypto/sha1"
 	"crypto/sha256"
 	"crypto/tls"
+	"crypto/x509"
+	"flag"
 	"fmt"
-	"math"
 	"os"
+	"runtime"
+	"strconv"
 	"strings"
 	"time"
+
+	"golang.org/x/net/idna"
 )
+
+const (
+	PROG_NAME = "tls_cert_info"
+	VERSION   = "0.8"
+)
+
+var (
+	cfgValidityDaysOnly bool
+	cfgHost             string
+	cfgPort             uint
+	hostStr, portStr    string
+	PKeyPKCS            [4]string  = [4]string{"Unknown", "RSA", "DSA", "ECDSA"}
+	SignPKCS            [13]string = [13]string{
+		"Unknown",
+		"MD2 With RSA",
+		"MD5 With RSA",
+		"SHA1 With RSA",
+		"SHA256 With RSA",
+		"SHA384 With RSA",
+		"SHA512 With RSA",
+		"DSA With SHA1",
+		"DSA With SHA256",
+		"ECDSA With SHA1",
+		"ECDSA With SHA256",
+		"ECDSA With SHA384",
+		"ECDSA With SHA512",
+	}
+)
+
+func init() {
+	usageStr := "Usage:\t%v [-r] <HOST>[:<PORT>]\n\t%v [-r] -H <HOST> [-P <PORT>]\n\n"
+	usageStr += "<HOST> might be a DNS name or IP address. IPv6 address should be enclosed\n"
+	usageStr += "by square brackets.\n\nCommand line parameters:\n"
+
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, usageStr, PROG_NAME, PROG_NAME)
+		flag.PrintDefaults()
+		fmt.Fprintf(os.Stderr, "  -h\tShow this help page.\n")
+	}
+}
 
 func main() {
 	// Parameters check
-	if len(os.Args) != 2 {
-		show_help()
+	commandLineGet()
+	if len(os.Args) < 2 {
+		flag.Usage()
+		os.Exit(0)
 	}
-	ssl_server := os.Args[1]
+
+	// Host and port converting to a server string
+	if len(cfgHost) == 0 { // <HOST>:<PORT> form used
+		hostStr, portStr = normalizeHostStr(os.Args[len(os.Args)-1])
+	} else { // -H <HOST> -P <PORT> form used
+		hostStr, portStr = normalizeHostStr(cfgHost + ":" + portStr)
+	}
+
+	if len(portStr) == 0 { // port was not specified
+		// "flag" already checked cfgPort for type of uint
+		portStr = strconv.Itoa(int(cfgPort))
+	}
 
 	// Connect to server
-	conn, err := tls.Dial("tcp", ssl_server, &tls.Config{InsecureSkipVerify: true})
+	serverStr := hostStr + ":" + portStr
+	tlsCfg := tls.Config{InsecureSkipVerify: true}
+	conn, err := tls.Dial("tcp", serverStr, &tlsCfg)
+	if err != nil && strings.Contains(err.Error(), "too many colons in address") {
+		// try to add [] and set default port
+		serverStr = "[" + os.Args[len(os.Args)-1] + "]:443"
+		conn, err = tls.Dial("tcp", serverStr, &tlsCfg)
+	}
 	if err != nil {
-		fmt.Printf("Canot connect to %v: %v\n", ssl_server, err)
+		fmt.Printf("Canot connect to %v: %v\n", serverStr, err)
 		os.Exit(1)
 	}
 	defer conn.Close()
-	fmt.Printf("Connected to %v\n", ssl_server)
 
-	// Show server's certificate info
-	crt := conn.ConnectionState().PeerCertificates[0]
-	days_left := math.Ceil(crt.NotAfter.Sub(time.Now()).Seconds()/86400) - 1
-	sha1Fingerprint := sha1.Sum(crt.Raw)
-	sha256Fingerprint := sha256.Sum256(crt.Raw)
-
-	infoStr := "*** Certificate info:\nVersion:\t%v\nHost:\t\t%v\n"
-	infoStr += "NotBefore:\t%v\nNotAfter:\t%v\n"
-	infoStr += "*** Fingerprints:\nsha1:\t\t%v\nsha256:\t\t%v\n"
-	fmt.Printf(infoStr, crt.Version, crt.DNSNames,
-		crt.NotBefore, crt.NotAfter,
-		byteSlice2Str(sha1Fingerprint[:]), byteSlice2Str(sha256Fingerprint[:]))
-	fmt.Printf("*** Note:\nIt is %v now: %v validity days left\n", time.Now(), days_left)
-}
-
-func show_help() {
-	fmt.Println("Usage:", os.Args[0], "<HOST>:<PORT>")
-	os.Exit(0)
+	if !cfgValidityDaysOnly {
+		fmt.Printf("Connected to %v\n", serverStr)
+	}
+	showCrtInfo(conn.ConnectionState().PeerCertificates[0])
 }
 
 // byteSlice2Str converts a slice of bytes to a colon delimited hexs string
 func byteSlice2Str(sl []byte) string {
 	strArr := make([]string, len(sl))
+
 	for i, b := range sl {
 		strArr[i] = fmt.Sprintf("%0.2x", b)
 	}
 	return strings.Join(strArr, ":")
+}
+
+func commandLineGet() {
+	flag.BoolVar(&cfgValidityDaysOnly, "r", false, "Print remaining validity days count only.")
+	flag.StringVar(&cfgHost, "H", "", "DNS host name or IP address.")
+	flag.UintVar(&cfgPort, "P", 443, "Port.")
+	flagVersion := flag.Bool("v", false, "Print version information and exit.")
+	flag.Parse()
+
+	if *flagVersion {
+		fmt.Println(PROG_NAME, "v.", VERSION, "["+runtime.Version()+" runtime]")
+		os.Exit(0)
+	}
+}
+
+// normalizeHostStr gets a string in form of <HOST>[:<PORT>]
+// and returs normalized DNS hostname and port strings
+func normalizeHostStr(hName string) (hStr, pStr string) {
+	// split host name and port if any
+	numArr := strings.Split(hName, ":")
+	switch len(numArr) {
+	case 1:
+		hStr = numArr[0]
+	case 2:
+		hStr = numArr[0]
+		pStr = numArr[1]
+	default:
+		hStr = strings.Join(numArr[:len(numArr)-1], ":")
+		pStr = numArr[len(numArr)-1]
+	}
+
+	// Check if host name is not an ASCII string
+	// and convert it to punycode if required
+	for _, c := range hStr {
+		if c > 127 {
+			var err error
+			hStr, err = idna.ToASCII(hStr)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Cannot convet %v to punycode\n")
+				os.Exit(1)
+			}
+			break
+		}
+	}
+	return
+}
+
+// Show server's certificate info
+func showCrtInfo(crt *x509.Certificate) {
+	days_left := int(crt.NotAfter.Sub(time.Now()).Seconds() / 86400)
+
+	if cfgValidityDaysOnly {
+		fmt.Println(days_left)
+		os.Exit(0)
+	}
+
+	sha1Fingerprint := sha1.Sum(crt.Raw)
+	sha256Fingerprint := sha256.Sum256(crt.Raw)
+
+	infoStr := "*** Certificate info:\nIssuerCN:\t%v\nVersion:\t%v\n"
+	infoStr += "SerialNum:\t%x\n"
+	infoStr += "PubKey:\t\t%v Encryption\n"
+	infoStr += "CrtSign:\t%v Encryption\n"
+	infoStr += "NotBefore:\t%v\nNotAfter:\t%v - %v days left\n"
+	infoStr += "SubjectCN:\t%v\nDNSNames:\t%v\n"
+	infoStr += "*** Fingerprints:\nsha1:\t\t%v\nsha256:\t\t%v\n"
+	fmt.Printf(infoStr, crt.Issuer.CommonName, crt.Version,
+		crt.SerialNumber,
+		PKeyPKCS[crt.PublicKeyAlgorithm],
+		SignPKCS[crt.SignatureAlgorithm],
+		crt.NotBefore, crt.NotAfter, days_left,
+		crt.Subject.CommonName, crt.DNSNames,
+		byteSlice2Str(sha1Fingerprint[:]), byteSlice2Str(sha256Fingerprint[:]))
 }
